0b4191eced1575c662bb910dca5c1727b7509305
HardManDev <hardman.dev@protonmail.com>
2022-10-12 04:48:40 +0300

    feat: completed "Extended repeater" task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,34 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
+class Repeater {
+  #result = []
+
+  constructor(str, { repeatTimes, separator = '+', addition = '', additionRepeatTimes, additionSeparator = '|' }) {
+    this.str = String(str);
+    this.repeatTimes = repeatTimes;
+    this.separator = separator;
+    this.addition = String(addition);
+    this.additionRepeatTimes = additionRepeatTimes;
+    this.additionSeparator = additionSeparator;
+  }
+
+  getResult() {
+    this.#result = new Array(this.repeatTimes).fill(this.str)
+
+    this.#result = [...this.#result].map(e =>
+        e += new Array(this.additionRepeatTimes)
+            .fill(this.addition)
+            .join(this.additionSeparator)
+    )
+
+    return this.#result.join(this.separator)
+  }
+
+  get result() {
+    return this.#result
+  }
+}
+
 /**
  * Create a repeating string based on the given parameters
  *  
@@ -15,9 +44,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  return new Repeater(str, options).getResult()
 }
 
 module.exports = {
