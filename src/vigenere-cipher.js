37ae5ee3591912e5bf74a2dd1c1a5e07bbf6b898
HardManDev <hardman.dev@protonmail.com>
2022-10-16 20:30:37 +0300

    feat: completed "Vigenere cipher" task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  #symbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+
+  constructor(isNotReverse = true) {
+    Array.prototype.getAnyway = function(index) {
+      const getAnyway = (index) => {
+        return index > 0
+            ? this.at(index) !== undefined ? this.at(index) : getAnyway(index - this.length)
+            : this.at(index) !== undefined ? this.at(index) : getAnyway(this.length - index - 1)
+      }
+
+      return getAnyway(index)
+    }
+
+    this.isNotReverse = isNotReverse
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(str, key) {
+    return this.#process('encrypt', str, key)
+  }
+
+  decrypt(str, key) {
+    return this.#process('decrypt', str, key)
+  }
+
+  #process(type, str, key) {
+    let offset = 0
+    const result = []
+
+    if (!(str && key))
+      throw new Error('Incorrect arguments!')
+
+    str = str.toUpperCase().split('')
+    key = key.toUpperCase().split('')
+
+    str.forEach((e, i) => {
+      if (!this.#symbols.includes(e)) {
+        offset++
+        result.push(e)
+        return
+      }
+
+      const strPos = this.#symbols.indexOf(e)
+      const keyPos = this.#symbols.indexOf(key.getAnyway(Math.abs(i - offset)))
+
+      switch (type) {
+        case 'encrypt':
+          result.push(this.#symbols.getAnyway(strPos + keyPos % this.#symbols.length))
+          break;
+        case 'decrypt':
+          result.push(this.#symbols.getAnyway(strPos - keyPos % this.#symbols.length))
+          break;
+      }
+    });
+
+    return (this.isNotReverse ? result : result.reverse()).join('')
   }
 }
 
